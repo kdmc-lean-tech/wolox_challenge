3755f0ccb77b894a064035bd8784ebf2a8d12bc8
kevin <kevin.melan00@usc.edu.co>
2021-05-16 16:35:25 -0500

    Add mock traslate service and path alias testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,13 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateService } from '@ngx-translate/core';
+import { TestTranslateService } from '@testing/services/traslate-service/test-traslate.service';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -11,12 +16,15 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+      providers: [
+        { provide: TranslateService, useClass: TestTranslateService }
+      ]
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
---
+++ src/testing/services/traslate-service/test-traslate.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable()
+export class TestTranslateService extends TranslateService {
+  constructor() {
+    super(null, null, null, null, null, null, null, null, null);
+  }
+
+  public setDefaultLang() {
+  }
+}
