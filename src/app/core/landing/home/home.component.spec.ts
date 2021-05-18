e35924d487782dda1690c34f106168f18592562b
Kevin David Melan <kmelan@lean-tech.io>
2021-05-17 21:44:10 -0500

    Add translate module and pipe in home spec

--- src/app/core/landing/home/home.component.spec.ts
+++ src/app/core/landing/home/home.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslateMockPipe } from '@testing/pipes/test-translate.pipe';
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +10,13 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [
+        HomeComponent,
+        TranslateMockPipe
+      ],
+      imports: [
+        TranslateModule
+      ]
     })
     .compileComponents();
   }));
