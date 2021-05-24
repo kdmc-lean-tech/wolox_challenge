dad9950aaa8c0d48032932587f41fed0cc4d528c
Kevin David Melan <kmelan@lean-tech.io>
2021-05-23 22:09:50 -0500

    Add technologies service mock in technology list component

--- src/app/core/dashboard/technology-list/technology-list.component.spec.ts
+++ src/app/core/dashboard/technology-list/technology-list.component.spec.ts
@@ -1,19 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TechnologyService } from '@services/technologies/technologies.service';
+import { TestTechnologiesService } from '@testing/services/technologies/test-technologies.service';
 
 import { TechnologyListComponent } from './technology-list.component';
 
 describe('TechnologyListComponent', () => {
   let component: TechnologyListComponent;
   let fixture: ComponentFixture<TechnologyListComponent>;
+  let technologyService: TechnologyService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TechnologyListComponent ]
+      declarations: [ TechnologyListComponent ],
+      providers: [
+        { provide: TechnologyService, useClass: TestTechnologiesService }
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    technologyService = TestBed.inject(TechnologyService);
     fixture = TestBed.createComponent(TechnologyListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
