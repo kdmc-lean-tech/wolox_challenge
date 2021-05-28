bce3a181b05a2f7ef3ea8f51bec98ba26651e907
Kevin David Melan <kmelan@lean-tech.io>
2021-05-27 20:22:44 -0500

    Add landing footer and benefits and minor changes in responsive styles

--- src/app/core/landing/landing.module.ts
+++ src/app/core/landing/landing.module.ts
@@ -4,12 +4,16 @@ import { LandingComponent } from '@core/landing/landing.component';
 import { LandingRoutingModule } from '@core/landing/landing-routing.module';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '@shared/shared.module';
+import { BenefitsComponent } from './benefits/benefits.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     LandingComponent,
-    HomeComponent
+    HomeComponent,
+    BenefitsComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
